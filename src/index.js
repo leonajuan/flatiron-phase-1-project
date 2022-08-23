941e1013b5911fa71540ae2e60ed550e5b066c37
Leona Juan <leonajuan@gmail.com>
2022-08-23 15:25:35 -0400

    put the fetches into functions + added mouseover and mouseout event listeners

--- src/index.js
+++ src/index.js
@@ -2,76 +2,75 @@ const test = document.getElementById("test")
 const featuredCocktails = document.getElementById("featured-cocktails-menu")
 const form = document.getElementById("song-form")
 const featuredCocktailsContainer = document.getElementById("featured-container")
-const featuredCocktailClick = document.getElementById('featured-cocktail')
+const featuredCocktailNav = document.getElementById('featured-cocktail-nav')
 const formDrinkContainer = document.querySelector(".form-drink")
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
-.then(res => res.json())
-.then(data => {
-  for(let i = 0; i < 5; i++){ //grab 5 drinks 
-    let drinksArray = data.drinks // assigns a value to the data 
-    let randomElement = drinksArray[Math.floor(Math.random() * drinksArray.length)] // grabs random drink
-    let drinkName = document.createElement("h5") // creates an element for each drink's name
-    featuredCocktails.append(featuredCocktailClick) //add individual cocktail elements to the cocktails menu
-    
-    //add and append an individual cocktail div to the overall cocktails container
-    let featuredCocktailDiv = document.createElement("div") 
-    featuredCocktailDiv.setAttribute("id", "featured-cocktails-div")
-    featuredCocktailsContainer.append(featuredCocktailDiv) 
+document.addEventListener("DOMContentLoaded", displayDrinks()) // calling displayDrinks function once DOM has loaded - will display five random drinks
 
-    //add and append drink ids and names to the individual cocktail elements
-    drinkName.setAttribute("id", "drink-name")
-    drinkName.innerText = randomElement.strDrink // adds the name of a random drink from the data 
-    featuredCocktailDiv.append(drinkName)// adds the drink name to the 'featured cocktails list'
-
-    //add and append drink images to the individual cocktail elements
-    let drinkImage = document.createElement("img")
-    drinkImage.setAttribute("id", "featured-drink-image")
-    drinkImage.src = randomElement.strDrinkThumb
-    featuredCocktailDiv.append(drinkImage)
-
-    //add event listener to each featured cocktail
-    featuredCocktailDiv.addEventListener("click", () => {
-      let id = randomElement.idDrink //get the ID of the random drink
-      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-      .then(res => res.json())
-      .then(data => {
-        let drinkRecipe = document.createElement("p")
-
-        
-        if(data.drinks[0].strIngredient3 == null){
-          drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1} and ${data.drinks[0].strIngredient2}. ${data.drinks[0].strInstructions}` 
-          featuredCocktailDiv.append(drinkRecipe)
-        } else if(data.drinks[0].strIngredient4 == null){
-          drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2} and ${data.drinks[0].strIngredient3}. ${data.drinks[0].strInstructions}` 
-          featuredCocktailDiv.append(drinkRecipe)
-        } else {
-          drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2}, ${data.drinks[0].strIngredient3} and ${data.drinks[0].strIngredient4}. ${data.drinks[0].strInstructions}` 
-          featuredCocktailDiv.append(drinkRecipe)
-        }
-        // switch(expression) {
-        //   case (data.drinks[0].strIngredient3 == null):
-        //     drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1} and ${data.drinks[0].strIngredient2}. ${data.drinks[0].strInstructions}` 
-        //     featuredCocktailDiv.append(drinkRecipe)
-        //     break;
-        //   case (data.drinks[0].strIngredient4 == null):
-        //     drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2} and ${data.drinks[0].strIngredient3}. ${data.drinks[0].strInstructions}` 
-        //     featuredCocktailDiv.append(drinkRecipe)
-        //     break;
-        //   default:
-        //     drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2}, ${data.drinks[0].strIngredient3} and ${data.drinks[0].strIngredient4}. ${data.drinks[0].strInstructions}` 
-        //     featuredCocktailDiv.append(drinkRecipe)
-        // }
-      })
-        // console.log(data.drinks[0].strInstructions))
-    })
-  } 
-})
-
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => { // form needs work - we will probably need to do another fetch to the cocktail database - easiest way will be to pick a letter and grab that API?
   e.preventDefault()
-  console.log(formDrinkContainer)
+  // displayDrinks()
+  // displayDrinkDetails()
 })
+
+function displayDrinks() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+  .then(res => res.json())
+  .then(data => {
+    for(let i = 0; i < 5; i++){ //grab 5 drinks
+      let drinksArray = data.drinks // assigns a value to the data
+      let randomElement = drinksArray[Math.floor(Math.random() * drinksArray.length)] // grabs random drink
+      let drinkName = document.createElement("h5") // creates an element for each drink's name
+      featuredCocktails.append(featuredCocktailNav) //add individual cocktail elements to the cocktails menu
+
+      let individualFeaturedCocktail = document.createElement("div") // creates a div for each featured cocktail
+      individualFeaturedCocktail.setAttribute("id", "individual-featured-cocktail") // assigns an id for each featured cocktail
+      featuredCocktailsContainer.append(individualFeaturedCocktail) // appends each featured cocktail to the full featured cocktails div
+  
+      drinkName.setAttribute("id", "drink-name") // assigns an id for the individual drink names
+      drinkName.innerText = randomElement.strDrink  // sets the text for the individual drink names
+      individualFeaturedCocktail.append(drinkName) // appends each drink name to the individual cocktails div
+  
+      let drinkImage = document.createElement("img") // creates an <img> for each individual drink
+      drinkImage.setAttribute("id", "featured-drink-image") // sets the id
+      drinkImage.src = randomElement.strDrinkThumb // grabs the source for each <img> from the API
+      individualFeaturedCocktail.append(drinkImage) // apends each drink image to the individual cocktails div
+      
+      individualFeaturedCocktail.addEventListener("mouseover", () => { // when the mouse hovers over each featured cocktail...
+        individualFeaturedCocktail.style.boxShadow = "6px 10px 14px 6px rgb(255,27,187)" // changes the card shadow
+      })
+
+      individualFeaturedCocktail.addEventListener("mouseout", () => { // when the mouse leaves each featured cocktail...
+        individualFeaturedCocktail.style.boxShadow = "0px 4px 8px 0px rgba(0,0,0,0.2)" // goes back to normal
+      })
+
+      individualFeaturedCocktail.addEventListener("click", () => { // when a featured cocktail is clicked...
+        displayDrinkDetails(randomElement.idDrink) // the displayDrinkDetails function is called which will show the drink ingredients + recipe
+      })
+ 
+    } 
+  })
+}
+
+function displayDrinkDetails(id) { // will display the featured drink's ingredients + recipe
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+  .then(res => res.json())
+  .then(data => {
+    let drinkRecipe = document.querySelector("#details")
+    
+    // if else based on how many ingredients are listed in the API
+    if(data.drinks[0].strIngredient3 == null){
+      drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1} and ${data.drinks[0].strIngredient2}. ${data.drinks[0].strInstructions}` 
+      // individualFeaturedCocktail.append(drinkRecipe)
+    } else if(data.drinks[0].strIngredient4 == null){
+      drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2} and ${data.drinks[0].strIngredient3}. ${data.drinks[0].strInstructions}` 
+      // individualFeaturedCocktail.append(drinkRecipe)
+    } else {
+      drinkRecipe.innerText = `You will need ${data.drinks[0].strIngredient1}, ${data.drinks[0].strIngredient2}, ${data.drinks[0].strIngredient3} and ${data.drinks[0].strIngredient4}. ${data.drinks[0].strInstructions}` 
+      // individualFeaturedCocktail.append(drinkRecipe)
+    }
+  })
+}
 
 // TO-DO
   // put fetch in a function so it can be called in the "click" event and "submit" event
